7f9eb4389513db37eba19b8f3a720868db63b569
NimishKatara <you@NimishKatara.com>
2024-10-12 18:46:49 +0530

    Updated code with recent changes

--- server.js
+++ server.js
@@ -7,26 +7,19 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// Serve static files (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Handle a new connection
+app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', (socket) => {
     console.log('A user connected.');
 
-    // Listen for chat messages from the client
+   
     socket.on('chatMessage', (msg) => {
-        // Emit the message to all clients
         io.emit('chatMessage', msg);
     });
-
-    // Handle user disconnection
     socket.on('disconnect', () => {
         console.log('A user disconnected.');
     });
 });
-
-// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
